5cf5165f508ff3e0e442e0848ed0f04bb28f9a66
Balamurugan <160770970+Balamurugan-7@users.noreply.github.com>
2024-05-23 10:19:48 +0530

    Time: 1 ms (99.32%) | Memory: 41.7 MB (91.01%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -2,13 +2,12 @@ class Solution {
     public boolean isValid(String word) {
         int wlen = word.length();
         if(wlen<3) return false;
-        Set<Character> set = new HashSet<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
         int vc=0;
         int cc=0;
         for(int i=0;i<wlen;i++)
         {
             char l = word.charAt(i);
-            if(set.contains(l))
+            if(l=='a' || l=='e' || l=='i' || l=='o' || l=='u' || l=='A' || l=='E' || l=='I' || l=='O' || l=='U')
             {
                 vc++;
             }
