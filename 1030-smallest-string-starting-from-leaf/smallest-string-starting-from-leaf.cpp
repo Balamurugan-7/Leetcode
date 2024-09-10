7787588ddc0ffe800f7d68acfb04cf73088d9044
Balamurugan <160770970+Balamurugan-7@users.noreply.github.com>
2024-09-10 11:37:21 +0530

    Time: 15 ms (32.70%) | Memory: 22.9 MB (17.52%) - LeetSync

---
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> dp;
+    // vector<string> dp;
+    char c = 'a';
+    string minstring = "";
+    void traverse(TreeNode* root,string s)
+    {
+        string t(1,c + root->val);
+        if(root->left)
+            traverse(root->left,t + s);
+        if(root->right)
+            traverse(root->right,t + s);
+        if(!root->left && !root->right)
+        {
+            dp.push_back(t+s);
+        }
+    }
+    string smallestFromLeaf(TreeNode* root) {
+        traverse(root,"");
+        for(auto& p: dp){
+            if(minstring == "" || minstring > p)
+                minstring = p;
+        }
+        return minstring;
+    }
+};
